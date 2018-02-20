73eca64d4c128eb121d16a113ec8d2186d1c98c4
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-20 22:21:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('article one is here bro!!';
+  res.send('article one is here bro!!');
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('article two is here bro!!';
+  res.send('article two is here bro!!');
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('article three is here bro!!';
+  res.send('article three is here bro!!');
 });
 
 app.get('/ui/style.css', function (req, res) {
