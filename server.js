9fb255bdd1f152cc4db3114d50026bef2cc43efc
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-24 12:20:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,13 +55,15 @@ function createtemplate(data){
      </body>
     </html>
 `
+return htmltemplate;
+    
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createtemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
