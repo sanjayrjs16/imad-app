ce29feae68e2676e3cde128e5714bdab7f582c6d
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-25 14:32:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -65,3 +65,4 @@ var nameInput=document.getElementById('name');
 var name= nameInput.value;
 request.open('GET','http://sanjayrjs16.imad.hasura-app.io/submit-name?name'+ name,true);
     request.send(null);
+};
