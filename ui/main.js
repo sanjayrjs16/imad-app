d90579abd8dd01bf4b7ea9f923e78c113132915f
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-25 13:19:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,3 +31,18 @@ button.onclick = function(){
     
      
 };
+
+var nameInput=document.getElementById('name');
+var name= nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick = function(){
+    var names =['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<list.length;i++){
+        list+= '<li>' + names[i] + '</li>';
+        
+    }
+var ul=document.getElementById('namelist');
+ul.innerHTML = list;
+    
+};
