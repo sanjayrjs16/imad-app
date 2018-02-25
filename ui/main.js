c14f4cf5f511eba0ddb7057c426c6aa25461c0a4
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-25 14:33:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,6 +63,6 @@ ul.innerHTML = list;
 };
 var nameInput=document.getElementById('name');
 var name= nameInput.value;
-request.open('GET','http://sanjayrjs16.imad.hasura-app.io/submit-name?name'+ name,true);
+request.open('GET','http://sanjayrjs16.imad.hasura-app.io/submit-name?name='+ name,true);
     request.send(null);
 };
