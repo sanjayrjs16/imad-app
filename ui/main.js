2de23ea066028ffeb835c3f0770123971132aff9
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-25 14:30:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,24 +43,20 @@ submit.onclick = function(){
             if(request.status === 200){
                var names =request.responseText;
                names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+= '<li>' + names[i] + '</li>';
+               var list='';
+               for(var i=0;i<names.length;i++){
+                    list+= '<li>' + names[i] + '</li>';
         
-    }
-var ul=document.getElementById('namelist');
-ul.innerHTML = list;
+                }
+            var ul=document.getElementById('namelist');
+            ul.innerHTML = list;
                           
             }
         }
         
     };
-    var names =['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+= '<li>' + names[i] + '</li>';
-        
-    }
+    
+    
 var ul=document.getElementById('namelist');
 ul.innerHTML = list;
     
