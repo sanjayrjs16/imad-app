cef561aa0548112722d0ab69fd15b0c4f301fe5d
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-25 12:45:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,20 @@ var button=document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
     
+    var request= new XMLHttpRequest();
     
-    counter = counter + 1;
-    var span=document.getElementById('count');
-    span.innerHTML = counter.toString(); 
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter =request.responseText;
+                var span=document.getElementById('count');
+    span.innerHTML = counter.toString();                
+            }
+        }
+    };
+    
+    request.open('GET','http://sanjayrjs16.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+     
 };
