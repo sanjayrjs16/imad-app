b62aa794c165c437385ad623f4c3716ff361db11
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-25 11:19:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,6 @@
 console.log('Loaded!');
 
-img=document.getElementById("madi");
-var marginLeft = 0;
-function moveLeft(){
-    marginLeft=marginLeft + 6;
-    img.style.marginLeft=marginLeft + "px";
-}
-img.onclick=function(){
-var interval=setInterval(moveLeft,30);
-   
-}
+
 var button=document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
