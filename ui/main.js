30a240177f1586f4111c9acf872aba219700d9c0
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-25 13:22:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,7 +38,7 @@ var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
     var names =['name1','name2','name3','name4'];
     var list='';
-    for(var i=0;i<list.length;i++){
+    for(var i=0;i<names.length;i++){
         list+= '<li>' + names[i] + '</li>';
         
     }
