5e954c86f0b66b2d2230cffdaf48231c3d74cf4c
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-02-25 11:11:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,3 +10,11 @@ img.onclick=function(){
 var interval=setInterval(moveLeft,30);
    
 }
+var button=document.getElementById('counter');
+button.onclick =function(){
+    
+    
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.InnerHTML = counter.toString(); 
+}
