7c5c76791010660595fc663b7c22a269609cc2f4
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-03-02 23:21:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.get('/articles/:articleName', function (req, res) {
      else
      {
          var articleData = result.rows[0];
-         res.send(createtemplate(articles[articleData]));
+         res.send(createtemplate(articleData));
          
      }
      }
