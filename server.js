d5e48c75d79bc7bcc17d510bd9eb95466c8f6d12
sanjayrjs16 <36541242+sanjayrjs16@users.noreply.github.com>
2018-03-13 18:19:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -160,12 +160,13 @@ app.get('/articles/:articleName', function (req, res) {
   
 });
 
-function hash(input){
-    var hashed=crypto.pbkdf2Sync();
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,100000, 512, 'sha512');
+    return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res){
     
-var hashedString = hash(req.params.input);
+var hashedString = hash(req.params.input,'shit-just-do-it');
 res.send(hashedSring);
     
 });
